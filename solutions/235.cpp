0f16f96dd62c85cdf14475ea8d1df83d39eba4bc
Tarek Dakhran <t.dakhran@gmail.com>
2021-09-17 19:18:40 +0200

    solution: 235. Lowest Common Ancestor of a Binary Search Tree

---
+++ solutions/235.cpp
@@ -0,0 +1,14 @@
+class Solution {
+ public:
+  TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    while (true) {
+      if (p->val > root->val and q->val > root->val) {
+        root = root->right;
+      } else if (p->val < root->val and q->val < root->val) {
+        root = root->left;
+      } else {
+        return root;
+      }
+    }
+  }
+};
