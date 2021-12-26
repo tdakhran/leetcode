7c4c93fb1159f2c48787ae76f2483a8e554e47b4
Tarek Dakhran <t.dakhran@gmail.com>
2021-12-26 18:21:03 +0100

    update: 973. K Closest Points to Origin

--- solutions/973.cpp
+++ solutions/973.cpp
@@ -5,15 +5,15 @@ class Solution {
  public:
   std::vector<std::vector<int>> kClosest(std::vector<std::vector<int>> &points,
                                          int k) {
-    std::priority_queue<std::pair<int, std::vector<int>>> pq;
+    std::priority_queue<std::pair<int, size_t>> pq;
 
     auto distance = [](const auto &point) {
       return point[0] * point[0] + point[1] * point[1];
     };
 
-    for (auto &point : points) {
-      auto d = distance(point);
-      pq.push({d, std::move(point)});
+    for (size_t pos = 0; pos < points.size(); ++pos) {
+      auto d = distance(points[pos]);
+      pq.push({d, pos});
       if (pq.size() > static_cast<size_t>(k)) {
         pq.pop();
       }
@@ -21,7 +21,7 @@ class Solution {
 
     std::vector<std::vector<int>> result;
     while (not pq.empty()) {
-      result.emplace_back(std::move(pq.top().second));
+      result.emplace_back(points[pq.top().second]);
       pq.pop();
     }
     return result;
