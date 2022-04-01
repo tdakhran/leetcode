3a8638f85b3a376d87288581c3168fd83e04ca15
Tarek Dakhran <t.dakhran@gmail.com>
2022-04-01 10:11:41 +0200

    update: 344. Reverse String

--- solutions/344.cpp
+++ solutions/344.cpp
@@ -4,9 +4,9 @@ class Solution {
  public:
   void reverseString(std::vector<char>& s) {
     size_t lo = 0;
-    size_t hi = s.size() - 1;
+    size_t hi = s.size();
     while (lo < hi) {
-      std::swap(s[lo++], s[hi--]);
+      std::swap(s[lo++], s[--hi]);
     }
   }
 };
