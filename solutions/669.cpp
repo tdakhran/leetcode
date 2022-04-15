38f6447c2b7634d19e5ecdbbecc1b0c5eb3a249c
Tarek Dakhran <t.dakhran@gmail.com>
2022-04-15 08:38:16 +0200

    solution: 669. Trim a Binary Search Tree

---
+++ solutions/669.cpp
@@ -0,0 +1,17 @@
+class Solution {
+ public:
+  TreeNode *trimBST(TreeNode *root, int low, int high) {
+    if (not root) {
+      return nullptr;
+    }
+    root->left = trimBST(root->left, low, high);
+    root->right = trimBST(root->right, low, high);
+    if (root->val < low) {
+      return root->right;
+    }
+    if (root->val > high) {
+      return root->left;
+    }
+    return root;
+  }
+};
