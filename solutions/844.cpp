f8fe45a7767a12b84114248c3f40d755af738d1b
Tarek Dakhran <t.dakhran@gmail.com>
2022-05-01 08:22:23 +0200

    update: 844. Backspace String Compare

--- solutions/844.cpp
+++ solutions/844.cpp
@@ -1,17 +1,17 @@
 #include <string>
 
 class Solution {
+  static inline constexpr char backspace = '#';
+
  public:
   bool backspaceCompare(std::string s, std::string t) {
     auto typed = [](const std::string &str) {
       std::string result;
       for (auto c : str) {
-        if (c == '#') {
-          if (not result.empty()) {
-            result.pop_back();
-          }
-        } else {
+        if (c != backspace) {
           result.push_back(c);
+        } else if (not result.empty()) {
+          result.pop_back();
         }
       }
       return result;
